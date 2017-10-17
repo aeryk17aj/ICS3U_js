44eeb7b36ddb591bf025b15e545ceee113acf9f9
Aeryk <aeryk17aj@gmail.com>
2017-10-17 01:06:38 -0400

    Last one hopefully

--- freeTime/snake/CodeTemplate.js
+++ freeTime/snake/CodeTemplate.js
@@ -1,14 +1,13 @@
 $(document).ready(function () {
+	document.body.onmousedown = function () { return false; }; // so page is unselectable
 
-	document.body.onmousedown = function () { return false; }; //so page is unselectable
-
-	//Canvas stuff
-	const canvas = $("#canvas")[0];
-	const ctx = canvas.getContext("2d");
-	const w = $("#canvas").width();
-	const h = $("#canvas").height();
+	// Canvas stuff
+	const canvas = $('#canvas')[0];
+	const ctx = canvas.getContext('2d');
+	const w = $('#canvas').width();
+	const h = $('#canvas').height();
 	let mx, my;
-	let game_loop;
+	let gameLoop;
 
 	let snakeHeadX, snakeHeadY;
 	let snakeSpeed;
@@ -23,21 +22,7 @@ $(document).ready(function () {
 	let debugMode;
 	let nx, ny;
 
-
-
-	/////////////////////////////////
-	////////////////////////////////
-	////////	GAME INIT
-	///////	Runs this code right away, as soon as the page loads.
-	//////	Use this code to get everything in order before your game starts
-	//////////////////////////////
-	/////////////////////////////
-	function init ()
-	{
-
-		//////////
-		///STATE VARIABLES
-
+	function init () {
 		snakeHeadX = 1;
 		snakeHeadY = 1;
 		snakeSpeed = 1;
@@ -49,46 +34,31 @@ $(document).ready(function () {
 		tileY = 1;
 		cellWidth = 20;
 		cellHeight = cellWidth;
-		gridLength = 30; //pixel length = 1 + (gridLength*(cellWidth+1))
-		gridHeight = 30; //pixel height = 1 + (gridHeight*(cellHeight+1))
+		gridLength = 30; // pixel length = 1 + (gridLength*(cellWidth+1))
+		gridHeight = 30; // pixel height = 1 + (gridHeight*(cellHeight+1))
 
 		fruitX = 0;
 		fruitY = 0;
 
-		//Internal
+		// Internal
 		started = false;
 		points = 0;
 		direction = 2; // 1-2-3-4 | left-up-right-down
 		debugMode = true;
 
-
-		//////////////////////
-		///GAME ENGINE START
-		//	This starts your game/program
-		//	"paint is the piece of code that runs over and over again, so put all the stuff you want to draw in here
-		//	"60" sets how fast things should go
-		//	Once you choose a good speed for your program, you will never need to update this file ever again.
-
-		if (typeof game_loop != "undefined") clearInterval(game_loop);
-		game_loop = setInterval(paint, 60);
+		if (typeof gameLoop !== 'undefined') clearInterval(gameLoop);
+		gameLoop = setInterval(paint, 60);
 	}
 
 	init();
 
-	///////////////////////////////////////////////////////
-	//////////////////////////////////////////////////////
-	////////	Main Game Engine
-	////////////////////////////////////////////////////
-	///////////////////////////////////////////////////
-	function paint ()
-	{
+	function paint () {
+		// Background (Gray)
+		ctx.fillStyle = '#888888';
+		ctx.fillRect(0, 0, w, h);
 
-		//Background (Gray)
-		ctx.fillStyle = "#888888";
-		ctx.fillRect(0,0, w, h);
-
-		//Game Grid (White Squares)
-		ctx.fillStyle = "#FFFFFF";
+		// Game Grid (White Squares)
+		ctx.fillStyle = '#FFFFFF';
 		for (let i = 0; i < gridLength; i++) {
 			tileX = 1 + (21 * i);
 			for (let j = 0; j < gridHeight; j++) {
@@ -100,31 +70,30 @@ $(document).ready(function () {
 		spawnSnake();
 		spawnFruit();
 
-		if (snakeHeadX == fruitX && snakeHeadY == fruitY) {
+		if (snakeHeadX === fruitX && snakeHeadY === fruitY) {
 			generateFruitCoords();
-			//snakeLength+=1;
+			// snakeLength+=1;
 			points += 1;
 		}
 
-		//Debug button to generate random fruit position
+		// Debug button to generate random fruit position
 		if (debugMode) {
-			ctx.fillStyle = "#009933";
+			ctx.fillStyle = '#009933';
 			ctx.fillRect(w - 21, h - 21, 20, 20);
 
-			ctx.fillStyle = "#0000FF";
+			ctx.fillStyle = '#0000FF';
 			ctx.fillRect(w - 42, h - 21, 20, 20);
 
-			ctx.fillStyle = "#990000";
+			ctx.fillStyle = '#990000';
 			ctx.fillRect(w - 63, h - 21, 20, 20);
 		}
-	}////////////////////////////////////////////////////////////////////////////////END PAINT/ GAME ENGINE
+	}
 
-	////////// FUNCTIONS
 	function spawnFruit () {
-		ctx.fillStyle = "#FF0000";
-		if (fruitX == 0 || fruitY == 0) {
+		ctx.fillStyle = '#FF0000';
+		if (fruitX === 0 || fruitY === 0)
 			generateFruitCoords();
-		}
+
 		ctx.fillRect(fruitX, fruitY, cellWidth, cellHeight);
 	}
 
@@ -132,10 +101,9 @@ $(document).ready(function () {
 		let snakeLength = 3;
 		snakeArray = [];
 		for (let i = snakeLength - 1; i >= 0; i--)
-		{
-			snakeArray.push({x:i, y:0});
-		}
-		ctx.fillStyle = "#000000";
+			snakeArray.push({ x: i, y: 0 });
+
+		ctx.fillStyle = '#000000';
 		if (snakeHeadX < (1 + ((cellWidth + 1) * 5)) || snakeHeadX > (1 + ((cellWidth + 1) * 25))) {
 			if (snakeHeadY < (1 + ((cellHeight + 1) * 5)) || snakeHeadY > 1 + ((cellHeight + 1) * 25)) {
 				if (!started) {
@@ -157,121 +125,51 @@ $(document).ready(function () {
 		snakeHeadY = 1 + ((cellHeight + 1) * 5) + ((cellHeight + 1) * (Math.round(Math.random() * (gridHeight - 10))));
 	}
 
-
-	////////// FUNCTIONS END
-
-	////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////
-	/////	MOUSE LISTENER
-	//////////////////////////////////////////////////////
-	/////////////////////////////////////////////////////
-
-
-
-
-
-
-	/////////////////
-	// Mouse Click
-	///////////////
 	canvas.addEventListener('click', function (evt) {
-		if (mx >= w - 21 && my >= h - 21 && mx <= w && my <= h) {
+		if (mx >= w - 21 && my >= h - 21 && mx <= w && my <= h)
 			generateFruitCoords();
-		}
+
 		if (mx >= w - 42 && my >= h - 21 && mx <= w - 21 && my <= h) {
-			console.log("Fruit X: " + fruitX);
-			console.log("Fruit Y: " + fruitY);
+			console.log('Fruit X: ' + fruitX);
+			console.log('Fruit Y: ' + fruitY);
 		}
+
 		if (mx >= w - 63 && my >= h - 21 && mx <= w - 42 && my <= h) {
-			console.log("Snake X: " + snakeHeadX);
-			console.log("Snake Y: " + snakeHeadY);
+			console.log('Snake X: ' + snakeHeadX);
+			console.log('Snake Y: ' + snakeHeadY);
 		}
-
-
 	}, false);
 
-	canvas.addEventListener ('mouseout', function () {}, false);
-	canvas.addEventListener ('mouseover', function () {}, false);
+	canvas.addEventListener('mouseout', function () {}, false);
+	canvas.addEventListener('mouseover', function () {}, false);
 
 	canvas.addEventListener('mousemove', function (evt) {
-		const mousePos = getMousePos(canvas, evt);
+		const { left, top } = canvas.getBoundingClientRect();
 
-		mx = mousePos.x;
-		my = mousePos.y;
+		mx = evt.clientX - left;
+		my = evt.clientY - top;
 	}, false);
-
-
-	function getMousePos (canvas, evt)
-	{
-		const rect = canvas.getBoundingClientRect();
-		return {
-			x: evt.clientX - rect.left,
-			y: evt.clientY - rect.top
-		};
-	}
-
-
-	///////////////////////////////////
-	//////////////////////////////////
-	////////	KEY BOARD INPUT
-	////////////////////////////////
-
-
-
 
 	window.addEventListener('keydown', function (evt) {
 		const key = evt.keyCode;
 
-		if (key == 37)
-		{
-			if (!(snakeHeadX == 1))
-			{
+		if (key === 37) {
+			if (!(snakeHeadX === 1))
 				snakeHeadX -= 21;
-			}
-		}
-		else if (key == 38)
-		{
-			if (!(snakeHeadY == 1))
-			{
+		} else if (key === 38) {
+			if (!(snakeHeadY === 1))
 				snakeHeadY -= 21;
-			}
-		}
-		else if (key == 39)
-		{
-			if (!(snakeHeadX == w - 21))
-			{
+		} else if (key === 39) {
+			if (!(snakeHeadX === w - 21))
 				snakeHeadX += 21;
-			}
-		}
-		else if (key == 40)
-		{
-			if (!(snakeHeadY == h - 21))
-			{
+		} else if (key === 40) {
+			if (!(snakeHeadY === h - 21))
 				snakeHeadY += 21;
-			}
 		}
-		if (key > 36 && key < 41) {
+
+		if (key > 36 && key < 41)
 			direction = key - 36;
-		}
+
 		console.log(key);
-
-	//p 80
-	//r 82
-	//1 49
-	//2 50
-	//3 51
-	//w 87
-	//a 65
-	//s 83
-	//d 68
-	//left 37
-	//up 38
-	//right 39
-	//down 40
-
 	}, false);
-
-
-
-
 });
